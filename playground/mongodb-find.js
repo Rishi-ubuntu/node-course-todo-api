61e30ee23a95da44a7fc9ab1e8a6808a63c6084c
Rishi-ubuntu <rk9862425966@gmail.com>
2017-12-02 14:47:22 +0530

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,22 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+  if (error) {
+     return console.log('Unable To Connect to MongoDb Server');
+  }
+  console.log('Connected To MongoDb Server');
+   db.collection('Users').find({
+     name: "Rohit" 
+    }).toArray().then((docs) => {
+      console.log(JSON.stringify(docs, undefined, 2));
+   }, (err) => {
+       console.log('Unable to fetch todos', err);
+   });
+  db.collection('Users').find({ name: "Rohit" }).count().then((count) => {
+    console.log(`Total = ${count}`);
+ }, (err) => {
+     console.log('Unable to fetch todos', err);
+ });
+  db.close();
+});
---
+++ playground/mongodbConnect.js
@@ -0,0 +1,30 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+  if (error) {
+     return console.log('Unable To Connect to MongoDb Server');
+  }
+  console.log('Connected To MongoDb Server');
+//   db.collection('Todos').insertOne({
+//     text: 'Something to do',
+//     complete: false
+//   }, (err, result) => {
+//     if (err) {
+//       return console.log('Unable to insert todo', err);
+//     }
+//     console.log(JSON.stringify(result.ops, undefined, 2))
+//   });
+
+// db.collection('Users').insertOne({
+//   name: 'Rohit',
+//   Age: 25,
+//   location: 'Shillong'
+// }, (err, res) => {
+//   if (err) {
+//    return console.log('Unable to insert User', err);
+//   }
+//   console.log(res.ops[0]._id.getTimestamp())
+// });
+  db.close();
+});
